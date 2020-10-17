9c6a9b25c4a14f8609195ae8d5da1751b4372125
Vincent <vincentyan8@hotmail.com>
2020-10-17 16:55:38 -0700

    front-end initit with button

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var router = express.Router();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index', {
-      title: 'HomePage', 
+      title: 'Media TextScribe', 
       people: people.profiles
     });
 });
@@ -34,6 +35,12 @@ app.get('/profile', (req, res) => {
       person,
     });
   });
+
+app.post('/totext', (req,res) =>{
+  var x = req.body;
+  console.log(x.address);
+  res.render('index', {title: x.address, people: people.profiles});
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -50,5 +57,4 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 module.exports = app;
